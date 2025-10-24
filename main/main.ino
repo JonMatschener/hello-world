cf891c8ad3967f8e23d064290c1aa8c87d0c3b4b
DomSpatH2P <dgs44@pitt.edu>
2025-10-24 12:05:59 -0400

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -4,22 +4,24 @@
 #include <DFRobotDFPlayerMini.h>
 #include <SPI.h>
 
-#define H 1 //hit pin
-#define S 2 //stand pin
-#define D 3 //double pin
-#define A 4 //audio pin
-#define P 5 //play button pin
-#define dispCS 6 // control select
-#define dispDC 7 //data select
-#define dispRST 8 //display reset
-#define spkRX 9 //speaker RX
-#define spkTX 10 //speaker TX
+
+#define H A0 //hit pin
+#define S A1 //stand pin
+#define D A2 //double pin
+#define P 2 //play button pin
+#define dispCS 3 // control select
+#define dispDC 4 //data select
+#define dispRST 5 //display reset
+#define spkRX 6 //speaker RX
+#define spkTX 7 //speaker TX
+
 
 const int moves[3] = {H,S,D};
+const double levels[3] = {0,0,0}; //voltage analogthresholds for hit, stand, double
 const double startTime = 3000; //start time in milliseconds
 const double timeMult = 0.98; //time multiplier each round to slow down time
-const char* suits[4] = {"♠", "♥", "♦", "♣"}; //suits
-const char* ranks[13] = {"2","3","4","5","6","7","8","9","🔟","J","Q","K","A"}; //ranks
+const String suits[4] = {"♠ ", "♥ ", "♦ ", "♣ "}; //suits
+const String ranks[13] = {"2","3","4","5","6","7","8","9","🔟","J","Q","K","A"}; //ranks
 int lives; //number of lives
 int score; //score
 double time; //current time between reactions
@@ -27,17 +29,25 @@ int index; //pointer to next card
 int order[52]; //order of cards
 int cards[52]; //deck
 
+Adafruit_ILI9341 tft = Adafruit_ILI9341(dispCS,dispDC,dispRST); //initalize display
+SoftwareSerial spkSerial(spkRX,spkTX); //serial for speaker
+DFRobotDFPlayerMini myDFPlayer;
+
 class Hand{
   public:
     int hand[10] = {0,0,0,0,0,0,0,0,0,0};
     int numCards = 0;
     bool soft = false; //ace in hand
     int total = 0; //total except first ace if any
+    String disp = "";
     int cardToNum(int card){
       int val=card%16;
       if(val==12) return 1; //ace
       if(val<=8) return val+2; //2 to 10 in index 0 to 8
       return 10; //J,Q,K
+    }
+    String dispCard(int card){ //convert card number to display
+      return (ranks[card%16] + suits[card/16]); //decode card to display
     }
     int deal(){
       index++; //increment card before return
@@ -46,13 +56,15 @@ class Hand{
       int val = cardToNum(draw); //get value of draw
       if(val==1 && !soft) soft=true; //set soft total for first ace
       else total += val;
+      disp += dispCard(draw); //add card to display
     }
     int newestCard(){return hand[numCards];}
     void clear(){
-      int hand[10] = {0,0,0,0,0,0,0,0,0,0};
-      int numCards = 0;
-      bool soft = false; //ace in hand
-      int total = 0; //soft total
+      for(int i=0;i<10;i++) hand[i]=0;
+      numCards = 0;
+      soft = false; //ace in hand
+      total = 0; //soft total
+      disp = "";
     }
     int bestTotal(){
       if(soft){
@@ -67,8 +79,12 @@ class Hand{
     }
 };
 
-char* dispCard(int card){
-  char* ret[2] = {}
+void updateDisp(Hand player,Hand dealer){ //show cards on display
+  tft.fillScreen(ILI9341_WHITE); //clear screen
+  tft.setTextColor(ILI9341_BLACK); //black text
+  tft.setCursor(0,0); //move cursor to top
+  tft.println(dealer.disp); //dealer hand at top
+  tft.println(player.disp); //player hand at bottom
 }
 
 //hard total moves
@@ -108,7 +124,7 @@ int await(double time){
   double start = millis(); //record current time for start
   while(millis() - start < time){
     for(int i=0;i<3;i++)
-    if(digitalRead(moves[i])==HIGH) return moves[i]; //read each pin for a move
+    if(analogRead(moves[i])>levels[i]) return moves[i]; //read each pin for a move
   }
   return 0;
 }
@@ -142,8 +158,16 @@ void setup() {
   pinMode(S, INPUT);
   pinMode(D, INPUT);
   pinMode(P, INPUT);
-  pinMode(A, OUTPUT);
-  Serial.begin(9600);
+  pinMode(dispCS,OUTPUT);
+  pinMode(dispDC,OUTPUT);
+  pinMode(dispRST,OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Check check!");
+
+  spkSerial.begin(9600);
+  myDFPlayer.setTimeOut(500);
+  myDFPlayer.volume(0);
+  myDFPlayer.EQ(0);
 }
 
 void loop() {
@@ -159,31 +183,45 @@ void loop() {
     //TODO: set up actual game environment (cards dealing)
     dealer.deal();//dealer draws
     player.deal(); player.deal(); //player draws twice
+    updateDisp(player,dealer);
     if(player.bestTotal()==21){ //check for natural blackjack
+      myDFPlayer.play(11);
+      Serial.println("Natural blackjack");
       continue;
     }
     int command = action(player,dealer); //create action
     switch(command){
       case S:
+        Serial.println("Stand it");
+        myDFPlayer.play(1);
         break;
       case H:
+        Serial.println("Hit it");
+        myDFPlayer.play(2);
         break;
       case D:
+        Serial.println("Double it");
+        myDFPlayer.play(3);
         break;
     }
     //tell action
     int act=await(time); //wait for action
     if(command==act){//if correct
       //TODO
+      Serial.println("points");
+      myDFPlayer.play(4);
       score++;
     }
     else{//if incorrect
       //TODO
+      Serial.println("life lost");
+      myDFPlayer.play(5);
       lives--;
     }
     switch(act){ //determine how rest of the game goes
       case S:
         while (dealer.dealerHits()) dealer.deal(); //keep dealing to dealer
+        updateDisp(player,dealer);
         break;
       case H:
         player.deal(); //deal to player
@@ -195,20 +233,28 @@ void loop() {
     }
     switch(whoWins(player,dealer)){ //check who wins after initial draw and after
       case 3: //player wins
+        Serial.println("player wins");
+        myDFPlayer.play(6);
         break;
       case 2: //dealer wins
+        Serial.println("dealer wins");
+        myDFPlayer.play(7);
         break;
       case 1: //push
+        Serial.println("push");
+        myDFPlayer.play(8);
         break;
       case 0: //nothing
         break;
     }
     time*=timeMult;
     if(score==99){
-
+      Serial.println("WIN");
+      myDFPlayer.play(9);
     }
     if(lives==0){
-
+      Serial.println("Game over");
+      myDFPlayer.play(10);
     }
   }
 }
